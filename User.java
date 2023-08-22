3a602c891c9cd64ce87eadb03cdd35fa2bae6e55
shaikhusseni <126853056+shaikhusseni@users.noreply.github.com>
2023-08-23 00:54:59 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,43 @@
+package springmvc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String useremail;
+	private String username ;
+	private String userpassword;
+	public String getUseremail() {
+		return useremail;
+	}
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserpassword() {
+		return userpassword;
+	}
+	public void setUserpassword(String userpassword) {
+		this.userpassword = userpassword;
+	}
+	@Override
+	public String toString() {
+		return "User [useremail=" + useremail + ", username=" + username + ", userpassword=" + userpassword + "]";
+	}
+	
+	
+	
+
+}
